ff2caf5df4aa73524b67b6ad504814f5dc3e7366
pc 17 e2-9 <damora.lorenzo.zero@gmail.com>
2023-11-28 12:56:20 +0100

    modifiche minori e scrittura della consegna

--- Cassa/Starter.cs
+++ Cassa/Starter.cs
@@ -1,16 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Cassa
 {
+	/*
+	 * articoli: codice, descrizione, prezzo unitario.
+	 * classe Articolo: metodo sconta() -5% in caso di carta fedeltà.
+	 * suddividere articoli in:
+	 * - articoli alimentari: anno di scadenza
+	 * - + Alimentari freschi: scadenza (in n giorni) (max 5).
+	 * - non alimentari: materiale principale, se è riciclabile
+	 * 
+	 * ridefinire il metodo sconta() in base alle seguenti regole:
+	 * - articoli alimentari: sconto +20% se l’anno di scadenza è quello attuale;
+	 * - alimentari freschi, sconto + (2*(6 - g))%
+	 * - non al: sconto +10% se materiale riciclabile. 
+	 * 
+	 * diagramma UML + programma windows form: simulare la cassa:
+	 * - inserire le info degli articoli, distinguendoli
+	 * - se il cliente dispone della tessera fedeltà sconto 5%
+	 * - applicare gli sconti
+	 * 
+	 * Terminato l’inserimento, visualizzare lo scontrino:
+	 * - nome articoli
+	 * - prezzo unitario
+	 * - eventuale sconto
+	 * - importo totale da pagare.
+	 * 
+	 * Utilizzare un array per memorizzare le informazioni degli articoli inseriti.
+	 */
 	internal static class Starter
 	{
-		/// <summary>
-		/// Punto di ingresso principale dell'applicazione.
-		/// </summary>
 		[STAThread]
 		static void Main()
 		{
