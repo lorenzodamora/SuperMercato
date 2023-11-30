31b44d0c0ed6aff43d68b42914b3cea4b471cb8f
pc 17 e2-9 <damora.lorenzo.zero@gmail.com>
2023-11-30 10:53:08 +0100

    2

--- Cassa/Classe Articolo/ArticoloAlimentare.cs
+++ Cassa/Classe Articolo/ArticoloAlimentare.cs
@@ -2,23 +2,23 @@
 {
 	internal class ArticoloAlimentare : Articolo
 	{
-		private System.DateTime _scadenza;
-		public System.DateTime DataScadenza
+		private CustomDate _scadenza;
+		public CustomDate DataScadenza
 		{
 			get => _scadenza;
 			private set => _scadenza = value;
 		}
 
-		public ArticoloAlimentare() : base() => DataScadenza = System.DateTime.Now;
+		public ArticoloAlimentare() : base() => DataScadenza = CustomDate.Today;
 
-		public ArticoloAlimentare(string codice, string descrizione, float prezzo, System.DateTime scadenza)
+		public ArticoloAlimentare(string codice, string descrizione, float prezzo, CustomDate scadenza)
 			: base(codice, descrizione, prezzo)
 			=> DataScadenza = scadenza;
 
 		public ArticoloAlimentare(ArticoloAlimentare ap) : this(ap.Codice, ap.Descrizione, ap.PrezzoUnitario, ap.DataScadenza) { }
 
 		//metodo Clone
-		new public ArticoloAlimentare Clone() => (ArticoloAlimentare)MemberwiseClone();
+		new public ArticoloAlimentare Clone() => (ArticoloAlimentare)MemberwiseClone(); //new ?
 
 		//metodo Equals
 		public override bool Equals(object obj) => Equals(obj as ArticoloAlimentare);
@@ -37,7 +37,7 @@
 		{
 			float ps = PrezzoUnitario;
 			if(c) ps -= ps * 5 / 100;
-			if(DataScadenza.Year == System.DateTime.Today.Year)
+			if(DataScadenza.Year == CustomDate.Today.Year)
 				ps -= ps * 20 / 100;
 			return (float)System.Math.Round(ps, 2);
 		}
---
+++ Cassa/CustomDate.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Cassa
+{
+	internal struct CustomDate
+	{
+		internal ushort wYear;
+
+		internal ushort wMonth;
+
+		internal ushort wDay;
+
+		internal CustomDate(int year, int month = 1, int day = 1)
+		{
+			wYear = (ushort)year;
+			wMonth = (ushort)month;
+			wDay = (ushort)day;
+		}
+
+		public static CustomDate Today
+		{
+			get
+			{
+				var data = DateTime.Today;
+				return new CustomDate(data.Year, data.Month, data.Day);
+			}
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is CustomDate date&&
+						 wYear==date.wYear&&
+						 wMonth==date.wMonth&&
+						 wDay==date.wDay;
+		}
+
+		public override int GetHashCode()
+		{
+			int hashCode = -370995715;
+			hashCode=hashCode*-1521134295+wYear.GetHashCode();
+			hashCode=hashCode*-1521134295+wMonth.GetHashCode();
+			hashCode=hashCode*-1521134295+wDay.GetHashCode();
+			return hashCode;
+		}
+
+		public static bool operator ==(CustomDate left, CustomDate right)
+		{
+			return left.wYear == right.wYear && left.wMonth == right.wMonth && left.wDay == right.wDay;
+		}
+		public static bool operator !=(CustomDate left, CustomDate right)
+		{
+			return left.wYear != right.wYear && left.wMonth != right.wMonth && left.wDay != right.wDay;
+		}
+
+		public int Year
+		{
+			get => wYear;
+			set => wYear = (ushort)value;
+		}
+		public int Month
+		{
+			get => wMonth;
+			set => wMonth = (ushort)value;
+		}
+		public int Day
+		{
+			get => wDay;
+			set => wDay = (ushort)value;
+		}
+
+	}
+}
