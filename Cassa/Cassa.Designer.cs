40fbe005840e3e796dca87a1f49f271f40cf60a3
lorenzodamora <damora.lorenzo.studente@itispaleocapa.it>
2023-12-05 08:14:11 +0100

    per lo più grafica

--- Cassa/Cassa.Designer.cs
+++ Cassa/Cassa.Designer.cs
@@ -30,13 +30,23 @@
 		{
 			this.Pagine = new System.Windows.Forms.TabControl();
 			this.PageAlimentare = new System.Windows.Forms.TabPage();
-			this.PageNonAlimentare = new System.Windows.Forms.TabPage();
 			this.SubPagine = new System.Windows.Forms.TabControl();
 			this.PageAlimentareNormale = new System.Windows.Forms.TabPage();
+			this.lbl_scadenza = new System.Windows.Forms.Label();
+			this.date_scadenza = new System.Windows.Forms.DateTimePicker();
+			this.btn_sendAlimentareSemplice = new System.Windows.Forms.Button();
 			this.PageAlimentareFresco = new System.Windows.Forms.TabPage();
+			this.txt_scadenzaFresco = new System.Windows.Forms.TextBox();
+			this.lbl_scadenzaFresco = new System.Windows.Forms.Label();
+			this.btn_sendAlimentareFresco = new System.Windows.Forms.Button();
+			this.PageNonAlimentare = new System.Windows.Forms.TabPage();
+			this.btn_riciclabile = new System.Windows.Forms.Button();
+			this.lbl_riciclabile = new System.Windows.Forms.Label();
+			this.txt_materiale = new System.Windows.Forms.TextBox();
+			this.lbl_materiale = new System.Windows.Forms.Label();
+			this.btn_sendNonAlimentare = new System.Windows.Forms.Button();
 			this.GraphicTitle = new System.Windows.Forms.Label();
 			this.label1 = new System.Windows.Forms.Label();
-			this.btn_send = new System.Windows.Forms.Button();
 			this.lbl_prezzo = new System.Windows.Forms.Label();
 			this.lbl_descrizione = new System.Windows.Forms.Label();
 			this.lbl_codice = new System.Windows.Forms.Label();
@@ -48,38 +58,32 @@
 			this.Pagine.SuspendLayout();
 			this.PageAlimentare.SuspendLayout();
 			this.SubPagine.SuspendLayout();
+			this.PageAlimentareNormale.SuspendLayout();
+			this.PageAlimentareFresco.SuspendLayout();
+			this.PageNonAlimentare.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// Pagine
 			// 
 			this.Pagine.Controls.Add(this.PageAlimentare);
 			this.Pagine.Controls.Add(this.PageNonAlimentare);
+			this.Pagine.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.830189F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.Pagine.Location = new System.Drawing.Point(10, 374);
 			this.Pagine.Name = "Pagine";
 			this.Pagine.SelectedIndex = 0;
-			this.Pagine.Size = new System.Drawing.Size(740, 361);
+			this.Pagine.Size = new System.Drawing.Size(355, 361);
 			this.Pagine.TabIndex = 0;
 			// 
 			// PageAlimentare
 			// 
 			this.PageAlimentare.Controls.Add(this.SubPagine);
-			this.PageAlimentare.Location = new System.Drawing.Point(4, 22);
+			this.PageAlimentare.Location = new System.Drawing.Point(4, 25);
 			this.PageAlimentare.Name = "PageAlimentare";
 			this.PageAlimentare.Padding = new System.Windows.Forms.Padding(3);
-			this.PageAlimentare.Size = new System.Drawing.Size(732, 335);
+			this.PageAlimentare.Size = new System.Drawing.Size(347, 332);
 			this.PageAlimentare.TabIndex = 0;
 			this.PageAlimentare.Text = "Alimentare";
 			this.PageAlimentare.UseVisualStyleBackColor = true;
-			// 
-			// PageNonAlimentare
-			// 
-			this.PageNonAlimentare.Location = new System.Drawing.Point(4, 22);
-			this.PageNonAlimentare.Name = "PageNonAlimentare";
-			this.PageNonAlimentare.Padding = new System.Windows.Forms.Padding(3);
-			this.PageNonAlimentare.Size = new System.Drawing.Size(732, 335);
-			this.PageNonAlimentare.TabIndex = 1;
-			this.PageNonAlimentare.Text = "NonAlimentare";
-			this.PageNonAlimentare.UseVisualStyleBackColor = true;
 			// 
 			// SubPagine
 			// 
@@ -88,28 +92,151 @@
 			this.SubPagine.Location = new System.Drawing.Point(6, 6);
 			this.SubPagine.Name = "SubPagine";
 			this.SubPagine.SelectedIndex = 0;
-			this.SubPagine.Size = new System.Drawing.Size(720, 323);
+			this.SubPagine.Size = new System.Drawing.Size(329, 323);
 			this.SubPagine.TabIndex = 0;
+			this.SubPagine.Selecting += new System.Windows.Forms.TabControlCancelEventHandler(this.SubPagine_Selecting);
 			// 
 			// PageAlimentareNormale
 			// 
-			this.PageAlimentareNormale.Location = new System.Drawing.Point(4, 22);
+			this.PageAlimentareNormale.Controls.Add(this.lbl_scadenza);
+			this.PageAlimentareNormale.Controls.Add(this.date_scadenza);
+			this.PageAlimentareNormale.Controls.Add(this.btn_sendAlimentareSemplice);
+			this.PageAlimentareNormale.Location = new System.Drawing.Point(4, 25);
 			this.PageAlimentareNormale.Name = "PageAlimentareNormale";
 			this.PageAlimentareNormale.Padding = new System.Windows.Forms.Padding(3);
-			this.PageAlimentareNormale.Size = new System.Drawing.Size(712, 297);
+			this.PageAlimentareNormale.Size = new System.Drawing.Size(321, 294);
 			this.PageAlimentareNormale.TabIndex = 0;
 			this.PageAlimentareNormale.Text = "Semplice";
 			this.PageAlimentareNormale.UseVisualStyleBackColor = true;
 			// 
+			// lbl_scadenza
+			// 
+			this.lbl_scadenza.AutoSize = true;
+			this.lbl_scadenza.Location = new System.Drawing.Point(7, 27);
+			this.lbl_scadenza.Name = "lbl_scadenza";
+			this.lbl_scadenza.Size = new System.Drawing.Size(68, 16);
+			this.lbl_scadenza.TabIndex = 30;
+			this.lbl_scadenza.Text = "Scadenza";
+			// 
+			// date_scadenza
+			// 
+			this.date_scadenza.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.150944F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.date_scadenza.Location = new System.Drawing.Point(10, 43);
+			this.date_scadenza.Name = "date_scadenza";
+			this.date_scadenza.Size = new System.Drawing.Size(210, 21);
+			this.date_scadenza.TabIndex = 23;
+			// 
+			// btn_sendAlimentareSemplice
+			// 
+			this.btn_sendAlimentareSemplice.Location = new System.Drawing.Point(226, 30);
+			this.btn_sendAlimentareSemplice.Name = "btn_sendAlimentareSemplice";
+			this.btn_sendAlimentareSemplice.Size = new System.Drawing.Size(84, 217);
+			this.btn_sendAlimentareSemplice.TabIndex = 22;
+			this.btn_sendAlimentareSemplice.Text = "Invia";
+			this.btn_sendAlimentareSemplice.UseVisualStyleBackColor = true;
+			this.btn_sendAlimentareSemplice.Click += new System.EventHandler(this.SendAlimentareSemplice_Click);
+			// 
 			// PageAlimentareFresco
 			// 
-			this.PageAlimentareFresco.Location = new System.Drawing.Point(4, 22);
+			this.PageAlimentareFresco.Controls.Add(this.txt_scadenzaFresco);
+			this.PageAlimentareFresco.Controls.Add(this.lbl_scadenzaFresco);
+			this.PageAlimentareFresco.Controls.Add(this.btn_sendAlimentareFresco);
+			this.PageAlimentareFresco.Location = new System.Drawing.Point(4, 25);
 			this.PageAlimentareFresco.Name = "PageAlimentareFresco";
 			this.PageAlimentareFresco.Padding = new System.Windows.Forms.Padding(3);
-			this.PageAlimentareFresco.Size = new System.Drawing.Size(712, 297);
+			this.PageAlimentareFresco.Size = new System.Drawing.Size(321, 294);
 			this.PageAlimentareFresco.TabIndex = 1;
 			this.PageAlimentareFresco.Text = "Fresco";
 			this.PageAlimentareFresco.UseVisualStyleBackColor = true;
+			// 
+			// txt_scadenzaFresco
+			// 
+			this.txt_scadenzaFresco.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txt_scadenzaFresco.Location = new System.Drawing.Point(10, 197);
+			this.txt_scadenzaFresco.Name = "txt_scadenzaFresco";
+			this.txt_scadenzaFresco.Size = new System.Drawing.Size(187, 31);
+			this.txt_scadenzaFresco.TabIndex = 31;
+			// 
+			// lbl_scadenzaFresco
+			// 
+			this.lbl_scadenzaFresco.AutoSize = true;
+			this.lbl_scadenzaFresco.Location = new System.Drawing.Point(7, 178);
+			this.lbl_scadenzaFresco.Name = "lbl_scadenzaFresco";
+			this.lbl_scadenzaFresco.Size = new System.Drawing.Size(241, 16);
+			this.lbl_scadenzaFresco.TabIndex = 30;
+			this.lbl_scadenzaFresco.Text = "Numero giorni scadenza dopo apertura";
+			// 
+			// btn_sendAlimentareFresco
+			// 
+			this.btn_sendAlimentareFresco.Location = new System.Drawing.Point(226, 30);
+			this.btn_sendAlimentareFresco.Name = "btn_sendAlimentareFresco";
+			this.btn_sendAlimentareFresco.Size = new System.Drawing.Size(84, 217);
+			this.btn_sendAlimentareFresco.TabIndex = 30;
+			this.btn_sendAlimentareFresco.Text = "Invia";
+			this.btn_sendAlimentareFresco.UseVisualStyleBackColor = true;
+			// 
+			// PageNonAlimentare
+			// 
+			this.PageNonAlimentare.Controls.Add(this.btn_riciclabile);
+			this.PageNonAlimentare.Controls.Add(this.lbl_riciclabile);
+			this.PageNonAlimentare.Controls.Add(this.txt_materiale);
+			this.PageNonAlimentare.Controls.Add(this.lbl_materiale);
+			this.PageNonAlimentare.Controls.Add(this.btn_sendNonAlimentare);
+			this.PageNonAlimentare.Location = new System.Drawing.Point(4, 25);
+			this.PageNonAlimentare.Name = "PageNonAlimentare";
+			this.PageNonAlimentare.Padding = new System.Windows.Forms.Padding(3);
+			this.PageNonAlimentare.Size = new System.Drawing.Size(347, 332);
+			this.PageNonAlimentare.TabIndex = 1;
+			this.PageNonAlimentare.Text = "NonAlimentare";
+			this.PageNonAlimentare.UseVisualStyleBackColor = true;
+			// 
+			// btn_riciclabile
+			// 
+			this.btn_riciclabile.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.150944F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btn_riciclabile.Location = new System.Drawing.Point(104, 158);
+			this.btn_riciclabile.Name = "btn_riciclabile";
+			this.btn_riciclabile.Size = new System.Drawing.Size(52, 33);
+			this.btn_riciclabile.TabIndex = 34;
+			this.btn_riciclabile.Text = "Si";
+			this.btn_riciclabile.UseVisualStyleBackColor = true;
+			// 
+			// lbl_riciclabile
+			// 
+			this.lbl_riciclabile.AutoSize = true;
+			this.lbl_riciclabile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbl_riciclabile.Location = new System.Drawing.Point(16, 155);
+			this.lbl_riciclabile.Name = "lbl_riciclabile";
+			this.lbl_riciclabile.Size = new System.Drawing.Size(82, 36);
+			this.lbl_riciclabile.TabIndex = 33;
+			this.lbl_riciclabile.Text = "Riciclabile?\r\nNo";
+			this.lbl_riciclabile.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// txt_materiale
+			// 
+			this.txt_materiale.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txt_materiale.Location = new System.Drawing.Point(20, 73);
+			this.txt_materiale.Name = "txt_materiale";
+			this.txt_materiale.Size = new System.Drawing.Size(204, 31);
+			this.txt_materiale.TabIndex = 32;
+			// 
+			// lbl_materiale
+			// 
+			this.lbl_materiale.AutoSize = true;
+			this.lbl_materiale.Location = new System.Drawing.Point(17, 57);
+			this.lbl_materiale.Name = "lbl_materiale";
+			this.lbl_materiale.Size = new System.Drawing.Size(63, 16);
+			this.lbl_materiale.TabIndex = 31;
+			this.lbl_materiale.Text = "Materiale";
+			// 
+			// btn_sendNonAlimentare
+			// 
+			this.btn_sendNonAlimentare.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.btn_sendNonAlimentare.Location = new System.Drawing.Point(236, 57);
+			this.btn_sendNonAlimentare.Name = "btn_sendNonAlimentare";
+			this.btn_sendNonAlimentare.Size = new System.Drawing.Size(84, 217);
+			this.btn_sendNonAlimentare.TabIndex = 30;
+			this.btn_sendNonAlimentare.Text = "Invia";
+			this.btn_sendNonAlimentare.UseVisualStyleBackColor = true;
 			// 
 			// GraphicTitle
 			// 
@@ -127,49 +254,43 @@
 			// label1
 			// 
 			this.label1.BackColor = System.Drawing.Color.DimGray;
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.30189F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.33962F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label1.ForeColor = System.Drawing.Color.White;
 			this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
 			this.label1.Location = new System.Drawing.Point(207, 46);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(127, 40);
+			this.label1.Size = new System.Drawing.Size(142, 45);
 			this.label1.TabIndex = 17;
 			this.label1.Text = "Articolo";
 			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
-			// btn_send
-			// 
-			this.btn_send.Location = new System.Drawing.Point(266, 114);
-			this.btn_send.Name = "btn_send";
-			this.btn_send.Size = new System.Drawing.Size(84, 217);
-			this.btn_send.TabIndex = 22;
-			this.btn_send.Text = "Invia";
-			this.btn_send.UseVisualStyleBackColor = true;
-			// 
 			// lbl_prezzo
 			// 
 			this.lbl_prezzo.AutoSize = true;
+			this.lbl_prezzo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.150944F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.lbl_prezzo.Location = new System.Drawing.Point(31, 221);
 			this.lbl_prezzo.Name = "lbl_prezzo";
-			this.lbl_prezzo.Size = new System.Drawing.Size(78, 13);
+			this.lbl_prezzo.Size = new System.Drawing.Size(97, 16);
 			this.lbl_prezzo.TabIndex = 21;
 			this.lbl_prezzo.Text = "Prezzo Unitario";
 			// 
 			// lbl_descrizione
 			// 
 			this.lbl_descrizione.AutoSize = true;
+			this.lbl_descrizione.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.150944F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.lbl_descrizione.Location = new System.Drawing.Point(31, 157);
 			this.lbl_descrizione.Name = "lbl_descrizione";
-			this.lbl_descrizione.Size = new System.Drawing.Size(62, 13);
+			this.lbl_descrizione.Size = new System.Drawing.Size(78, 16);
 			this.lbl_descrizione.TabIndex = 20;
 			this.lbl_descrizione.Text = "Descrizione";
 			// 
 			// lbl_codice
 			// 
 			this.lbl_codice.AutoSize = true;
+			this.lbl_codice.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.150944F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.lbl_codice.Location = new System.Drawing.Point(31, 98);
 			this.lbl_codice.Name = "lbl_codice";
-			this.lbl_codice.Size = new System.Drawing.Size(78, 13);
+			this.lbl_codice.Size = new System.Drawing.Size(98, 16);
 			this.lbl_codice.TabIndex = 19;
 			this.lbl_codice.Text = "Codice Articolo";
 			// 
@@ -193,9 +314,10 @@
 			// lbl_amount
 			// 
 			this.lbl_amount.AutoSize = true;
+			this.lbl_amount.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.150944F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.lbl_amount.Location = new System.Drawing.Point(31, 281);
 			this.lbl_amount.Name = "lbl_amount";
-			this.lbl_amount.Size = new System.Drawing.Size(47, 13);
+			this.lbl_amount.Size = new System.Drawing.Size(56, 16);
 			this.lbl_amount.TabIndex = 26;
 			this.lbl_amount.Text = "Quantità";
 			// 
@@ -224,7 +346,6 @@
 			this.Controls.Add(this.txt_prezzo);
 			this.Controls.Add(this.lbl_amount);
 			this.Controls.Add(this.txt_codice);
-			this.Controls.Add(this.btn_send);
 			this.Controls.Add(this.lbl_prezzo);
 			this.Controls.Add(this.lbl_descrizione);
 			this.Controls.Add(this.lbl_codice);
@@ -237,6 +358,12 @@
 			this.Pagine.ResumeLayout(false);
 			this.PageAlimentare.ResumeLayout(false);
 			this.SubPagine.ResumeLayout(false);
+			this.PageAlimentareNormale.ResumeLayout(false);
+			this.PageAlimentareNormale.PerformLayout();
+			this.PageAlimentareFresco.ResumeLayout(false);
+			this.PageAlimentareFresco.PerformLayout();
+			this.PageNonAlimentare.ResumeLayout(false);
+			this.PageNonAlimentare.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -252,7 +379,7 @@
 		private System.Windows.Forms.TabPage PageAlimentareFresco;
 		private System.Windows.Forms.Label GraphicTitle;
 		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Button btn_send;
+		private System.Windows.Forms.Button btn_sendAlimentareSemplice;
 		private System.Windows.Forms.Label lbl_prezzo;
 		private System.Windows.Forms.Label lbl_descrizione;
 		private System.Windows.Forms.Label lbl_codice;
@@ -261,6 +388,15 @@
 		private System.Windows.Forms.Label lbl_amount;
 		private System.Windows.Forms.TextBox txt_prezzo;
 		private System.Windows.Forms.TextBox txt_amount;
+		private System.Windows.Forms.Button btn_sendAlimentareFresco;
+		private System.Windows.Forms.Button btn_sendNonAlimentare;
+		private System.Windows.Forms.DateTimePicker date_scadenza;
+		private System.Windows.Forms.TextBox txt_scadenzaFresco;
+		private System.Windows.Forms.Label lbl_scadenzaFresco;
+		private System.Windows.Forms.Label lbl_scadenza;
+		private System.Windows.Forms.Label lbl_riciclabile;
+		private System.Windows.Forms.TextBox txt_materiale;
+		private System.Windows.Forms.Label lbl_materiale;
+		private System.Windows.Forms.Button btn_riciclabile;
 	}
 }
-
--- Cassa/Cassa.cs
+++ Cassa/Cassa.cs
@@ -5,9 +5,165 @@ namespace Cassa
 {
 	public partial class Cassa : Form
 	{
+		struct Scontrino
+		{
+			internal Articolo[] _articoli;
+			internal int[] _amount;
+			public int count;
+			public int size;
+
+			public Scontrino(int size)
+			{
+				_articoli = new Articolo[size];
+				_amount = new int[size];
+				count = 0;
+				this.size = size;
+			}
+
+			public Articolo this[int index]
+			{
+				get
+				{
+					if(index >= 0 && index < count+1)
+						return _articoli[index];
+					else
+						throw new IndexOutOfRangeException("Index out of range.");
+				}
+				set
+				{
+					if(index >= 0 && index < count+1)
+						_articoli[index] = value;
+					else
+						throw new IndexOutOfRangeException("Index out of range.");
+				}
+			}
+
+			public int GetAmount(int index)
+			{
+				if(index >= 0 && index < count + 1)
+					return _amount[index];
+				else
+					throw new IndexOutOfRangeException("Index out of range.");
+			}
+			public void SetAmount(int index, int amount)
+			{
+				if(index >= 0 && index < count + 1)
+					_amount[index] = amount;
+				else
+					throw new IndexOutOfRangeException("Index out of range.");
+			}
+
+			private static int GetLength(int tot)
+			{
+				if(tot < 20) return 20;
+				return tot/20 * 20 + 20;
+			}
+			private static void Resize(ref Scontrino scontrino, int newSize)
+			{
+				if(scontrino.size == newSize) return;
+
+				Scontrino s2 = new Scontrino(newSize);
+				if(scontrino.size < newSize) //se newsize è più grande, copia fino ad array.length e il resto rimane default
+					for(int i = 0; i < scontrino.size; ++i)
+						s2.Add(scontrino[i], scontrino.GetAmount(i));
+				else //se newsize è più piccolo copia fino a newsize
+					for(int i = 0; i < newSize; i++)
+						s2.Add(scontrino[i], scontrino.GetAmount(i));
+				s2.size = newSize;
+				scontrino = s2;
+			}
+
+			public void Add(Articolo articolo, int amount)
+			{
+				if(count == size)
+					Resize(ref this, GetLength(size));
+				_articoli[count] = articolo;
+				_amount[count++] = amount;
+			}
+		}
+
+		Scontrino scontrino = new Scontrino(20);
+
 		public Cassa()
 		{
 			InitializeComponent();
+
+		}
+
+		/** Switch grafico del dateTimePicker */
+		void SubPagine_Selecting(object sender, TabControlCancelEventArgs e)
+		{
+			if((sender as TabControl).SelectedTab == PageAlimentareFresco)
+			{
+				PageAlimentareNormale.Controls.Remove(lbl_scadenza);
+				PageAlimentareNormale.Controls.Remove(date_scadenza);
+				PageAlimentareFresco.Controls.Add(lbl_scadenza);
+				PageAlimentareFresco.Controls.Add(date_scadenza);
+				return;
+			}
+			PageAlimentareFresco.Controls.Remove(lbl_scadenza);
+			PageAlimentareFresco.Controls.Remove(date_scadenza);
+			PageAlimentareNormale.Controls.Add(lbl_scadenza);
+			PageAlimentareNormale.Controls.Add(date_scadenza);
+		}
+
+		private void SendAlimentareSemplice_Click(object sender, EventArgs e)
+		{
+			if(CheckArticolo()) return;
+
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns>true se c'è errore</returns>
+		private bool CheckArticolo()
+		{
+			string errore1 = "Errore nell'inserimento";
+			string codice = txt_codice.Text.Trim();
+			string prezzo = txt_prezzo.Text.Trim();
+			if(codice == "" || txt_descrizione.Text == "" || prezzo == "")
+			{
+				MessageBox.Show("Riempi almeno le TextBox: Codice, Descrizione, Prezzo.", errore1);
+				return true;
+			}
+			if(!System.Linq.Enumerable.All(codice, char.IsDigit))
+			{
+				MessageBox.Show("Il Codice deve essere solo numerico [0-9]", errore1);
+				return true;
+			}
+
+			/// funzione locale
+			bool IsValidPrice(string input)
+			{
+				/**
+				 * Espressione regolare per un prezzo con massimo due cifre decimali
+				 * Ecco una spiegazione dettagliata:
+				 * - ^: Rappresenta l'inizio della stringa.
+				 * - \d+: Corrisponde a uno o più cifre (0-9).
+				 * - (: Inizia un gruppo di cattura.
+				 * - \.: Corrisponde a un punto decimale.
+				 * - \d{1,2}: Corrisponde a una o due cifre decimali.
+				 * - ): Chiude il gruppo di cattura.
+				 * - ?: Indica che il gruppo di cattura precedente (il punto decimale seguito da una o due cifre decimali) può essere presente o assente.
+				 * - $: Rappresenta la fine della stringa.
+				 */
+				string pattern = @"^\d+(\.\d{1,2})?$";
+				// Verifica se la stringa corrisponde al pattern
+				if(System.Text.RegularExpressions.Regex.IsMatch(input, pattern))
+				{
+					// Converte la stringa in un numero per ulteriori controlli
+					if(float.TryParse(input, out float price))
+					{
+						// Controlla che il numero sia positivo
+						return price > 0;
+					}
+				}
+				return false;
+			}
+
+			if(IsValidPrice(prezzo))
+			return false;
 		}
 	}
 }
